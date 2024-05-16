6406b3c4ada712c31a9bf49dbd98766d494cd597
Levvonci <lvnci@tutanota.com>
2024-05-16 21:08:40 +0200

    wait

---
+++ readFile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int numero;
+    FILE *file;
+
+    if ((file = fopen("/Users/lvnc/repo/SOR-Code/prova2.txt", "r")) == NULL) {
+        perror("Errore nella lettura\n");
+        return 1;
+    }
+
+    fscanf(file, "%d", &numero);
+    printf("Il valore di numero è: %d\n", numero);
+    fclose(file);
+
+    return 0;
+}
